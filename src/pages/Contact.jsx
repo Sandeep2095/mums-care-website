d009a2d3590d5f11436ed2473e3df4ca0ddafa67
Sandeep Sharma <sks34246@gmail.com>
2023-06-19 07:38:07 +0530

    iframe

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -123,6 +123,7 @@ const Contact = () => {
 
 					<div className='contact map'>
 						<iframe
+							title='myMap'
 							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7196.46184715684!2d85.15209209544722!3d25.597241735683806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed590d1ed3866f%3A0x9a2daaf5ef3be417!2sMUM&#39;S%20CARE!5e0!3m2!1sen!2sin!4v1687075710633!5m2!1sen!2sin'
 							width='600'
 							height='450'
